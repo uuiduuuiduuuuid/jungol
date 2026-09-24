5f99593d23b32a1ca4c16e08ac1431acca31cf5e
uuiduuuiduuuuid <binarycodeoption2@gmail.com>
2026-09-24 04:35:38 +0000

    awefawefawef

---
+++ 3238.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define compress(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+#define sz(x) (int)(x).size()
+#pragma GCC optimize ("O3,unroll-loops")
+#pragma GCC target ("avx,avx2,fma")
+#define endl '\n'
+
+void solve() {
+    
+}
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
